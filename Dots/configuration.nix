96c49f538d304e9071b9ff443878e7a263130dc9
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-09-29 21:22:42 +0300

    Update configuration.nix

--- Dots/configuration.nix
+++ Dots/configuration.nix
@@ -2,186 +2,146 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
+# The first line ({ config, pkgs, ... }:) denotes that this is actually a function that takes at least the two arguments config and pkgs.
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-     /etc/nixos/hardware-configuration.nix
+	imports =
+    [ 	# Include the results of the hardware scan.
+    	/etc/nixos/hardware-configuration.nix
     ];
-  environment.variables = {
-	SUDO_EDITOR = "nvim";
-    };
+    
+	# Select internationalisation properties.
+	# i18n.defaultLocale = "en_US.UTF-8";
+	# console = {
+	#   font = "Lat2-Terminus16";
+	#   keyMap = "us";
+	# };
   
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
-   time.timeZone = "America/New_York";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp2s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+	# Some programs need SUID wrappers, can be configured further or are
+	# started in user sessions.
+	# programs.mtr.enable = true;
+	# programs.gnupg.agent = {
+	#   enable = true;
+	#   enableSSHSupport = true;
+	# };
+  	
+	nixpkgs.config.allowUnfree = true;
+	time.timeZone = "Europe/Athens";
   
-    # BSPWM
-  services.xserver.windowManager.bspwm.enable = true;
- 
-  #zsh
-  programs.zsh.enable = true;
-  nixpkgs.config.allowUnfree = true;
-  # Configure keymap in X11
-   services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-  # Picom Fork
-  
-  # Enable CUPS to print documents.
-   services.printing.enable = true;
-
-  # Enable sound.
-   sound.enable = true;
-   hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-   services.xserver.libinput.enable = true;
-   
-     # Fonts
-  fonts.fonts = with pkgs; [
-  (nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
-];
- 
- 
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.nihar = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-   };
-   
-      users.extraUsers.nihar = {
- 
- 	shell = pkgs.zsh;
- 
-   };
-
+## User
+	users = {
+		users.sfikas = {
+			isNormalUser = true;
+			extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+			packages = with pkgs; {
+				feh;								# Image viewer and Wallpaper Setter
+				kitty;								# Terminal Emulator
+				fish;								# Shell 
+				xdragon;							# Drag and Drop utility from terminal
+				pavucontrol;						# Gui for Pulseaudio Volume Control
+				xclip;								# X Clipboard Manager
+				mpv;								# Video player
+				discord;							# Social Platform
+				brave;								# Browser
+				vscode-with-extensions;				# Proprietary Version of vscode
+			};
+		};
+		extraUsers.nihar = {
+			shell = pkgs.fish;
+		};
+	};
+		
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-      environment.systemPackages = with pkgs; [
-     neovim
-     wget
-     firefox
-     pfetch
-     git
-     feh
-     sxiv
-     #picom
-     nur.repos.reedrw.picom-next-ibhagwan
-     bspwm
-     sxhkd
-     alacritty
-     zsh
-     polybar
-     rofi
-     pipes
-     ranger
-     discord
-     pavucontrol
-     maim
-     killall
-     audacity
-     google-chrome
-     mumble
-     xclip
-     obs-studio
-     ffmpeg
-     mpv
-     gimp
-     gnome-breeze
-     lxappearance
-     kdenlive
-     youtube-dl
-     vscode
-     unzip
-   ];
+
+## Envorinment
+	environment.variables = {
+		SUDO_EDITOR = "nvim";
+	};
+	
+	environment.systemPackages = with pkgs; {		# System Packages:
+		neovim;										# Editor
+		wget;										#
+		zip;										#
+		unzip;										#
+		unrar;										#
+		git;										#
+		fira-code;									# Monospace font
+		bspwm;										# Window Manager
+		sxhkd;										# Bspwm Shortcuts Configuration
+		
+   };
    
-      # Auto upgr
-      # ades
-  system.autoUpgrade.enable = true;
-#  system.autoUpgrade.allowReboot = true;
- 
- 
-   nix = {
-    # Hard link identical files in the store automatically
-    autoOptimiseStore = true;
-    # automatically trigger garbage collection
-    gc.automatic = true;
-    gc.dates = "weekly";
-    gc.options = "--delete-older-than 30d";
-  };
- 
- 
+## Sound.
+	sound.enable = true;
+	hardware.pulseaudio.enable = true;
+     
+## Fonts
+	fonts.fonts = with pkgs; [
+		(nerdfonts.override { fonts = [ "FiraCode" ]; })
+	];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+## Services
+	services = {
+		openssh.enable = true;						# Enable the OpenSSH daemon.
+		xserver.enable = true;						# Enable the X11 Windowing System.
+		xserver.layout = "us";						# Configure keymap in X11
+		xserver.windowManager.bspwm.enable = true;	# BSPWM
+	#	xserver.libinput.enable = true;				# Touchpad Support.
+	#	printing.enable = true;						# Enables CUPS for printers Support.
+	};
+  
+## Networking
+	networking.hostName = "SF-nxos"
+	networking.firewall = {
+  		enable = false;								# Disables the firewall altogether.
+    #	allowedTCPPorts = [  ];						# Open ports in the firewall.
+ 	#	allowedUDPPorts = [  ];
+	};
 
-  # List services that you want to enable:
+	networking.networkmanager.enable = true;		# Enables networkManager.
+	# networking.wireless.enable = true;  			# Enables wireless support via wpa_supplicant.
+  
+	networking.useDHCP = false;						# The global useDHCP flag is deprecated, therefore explicitly set to false here.
+	networking.interfaces.enp2s0.useDHCP = true;	# Per-interface useDHCP will be mandatory in the future.
+  
+	# Configure network proxy if necessary
+	# networking.proxy.default = "http://user:password@proxy:port/"; 
+	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  
+## Bootloader
+	boot.loader = {
+		systemd-boot = {
+			enable = true;							# Enables the systemd-boot EFI boot loader.
+			configurationLimit = 5;					# Number of configurations at Boot Time.
+			editor = false;							# Disables editing of kernelparameters before Boot. 
+		};
+		efi.canTouchEfiVariables = true;
+	};
 
-  # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+## System
+	# system.autoUpgrade.enable = true;				# Auto upgrades
+	# system.autoUpgrade.allowReboot = true;
+	system.stateVersion = "21.05"; 					# Keep the default generated Value
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
-
+##	**NOTES**	##
+/*
+	   Various notes about Nix language:
+	 # `let` is recursive and lazy: `let x = x; in x` gives error about infinite
+		recursion; `let x = x; in 5` just evaluates to `5`.
+	 # `with foo; ...` still keeps `foo` visible inside the `...` code.
+	 # `{ inherit (foo.bar) baz xyzzy; }` is shortcut for `{ baz = foo.bar.baz;
+		xyzzy = foo.bar.xyzzy; }`.
+	 # `import ./foo/bar.nix arg1 arg2` loads code from ./foo/bar.nix and then
+		calls it as a function with arguments `arg1` and `arg2`.
+	 # `foo.bar or baz` returns `foo.bar` if `foo` has attribute `bar`, or
+		expression `baz` otherwise. In other words, `baz` is a "default value" if
+		`.bar` is missing.
+	 # `"foo ${bar.baz}"` evaluates to `bar.baz.outPath` string if present (see also
+		builtins.toString and [nix pill 6]
+		(http://lethalman.blogspot.com/2014/07/nix-pill-6-our-first-derivation.html)).
+	 # `foo ? bar` returns true if foo contains attribute bar.
+*/
 }
